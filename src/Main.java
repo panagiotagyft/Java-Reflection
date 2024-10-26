cc518e2b326e2beb2af521e6bb4f63a19cb2e729
panagiotagyft <giotagiota20001@gmail.com>
2024-10-26 22:56:15 +0300

    Main class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import modules.Config;
+
+public class Main{
+    
+    public static void main(String[] args) {
+
+        Config config = create_config(argc);
+    }
+
+}
