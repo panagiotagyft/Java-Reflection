bfefe98c02f784d6c715a443554a97204b498d95
panagiotagyft <giotagiota20001@gmail.com>
2024-10-26 22:56:02 +0300

    Configuration

---
+++ modules/Config.java
@@ -0,0 +1,10 @@
+public class Config {
+
+    public create_config(String[] args){
+        if(args.length==0){ 
+                System.out.println("Missing arguments...\n");
+                return;
+        }
+        
+    }
+}
