9144a467e6f2f92ef6b4a7c9916436ab47f33887
panagiotagyft <giotagiota20001@gmail.com>
2024-10-26 23:32:09 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import java.io.IOException;
 import modules.Config;
 
-public class Main{
+public class Main {
     
     public static void main(String[] args) {
-
-        Config config = create_config(argc);
+        try {
+            Config config = new Config();
+            config.create_config(args);
+        } catch (IOException e) {
+            System.out.println("I/O error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("An unexpected error: " + e.getMessage());
+        }
     }
-
 }
