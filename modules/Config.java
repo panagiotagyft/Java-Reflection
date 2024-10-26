15b15e4944fa1bca179294b6af5b4c3399d02335
panagiotagyft <giotagiota20001@gmail.com>
2024-10-27 00:02:02 +0300

    config

--- modules/Config.java
+++ modules/Config.java
@@ -1,8 +1,26 @@
 package modules;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class Config {
+
+    private String input_file;
+    private String output_file;
+    private int value_of_N;
+
+    public static boolean isValidFilePath(String pathStr) {
+        try {
+            Path path = Paths.get(pathStr);
+            return Files.exists(path) && Files.isRegularFile(path);
+        } catch (Exception e) {
+            System.out.println("This is not a valid file path.");
+            return false;
+        }
+    }
 
     public void create_config(String[] args) throws IOException {
         
@@ -14,9 +32,24 @@ public class Config {
             System.out.println("Too many arguments...\n");
             System.exit(1);
         }
-        else{
-            System.out.println("Success!\n");
+        // else{
+        //     System.out.println("Success!\n");
+        // }
+
+        // Error: One or more arguments are not valid integers
+        input_file = args[0];  output_file = args[1];
+        if (!isValidFilePath(input_file) || !isValidFilePath(output_file)) 
+            System.exit(1);
+                   
+        
+        // -- value_of_N
+        try {
+            value_of_N = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            System.out.println("This String: " + args[2] + ", is not a valid integer.");
         }
+                
     }
+
 }
 
